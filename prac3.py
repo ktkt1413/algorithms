7e8a25cf53a1256f2c59037c20eeb111b5c9a1e9
JungDaGyeom <ktkt1413@naver.com>
2024-01-03 12:06:03 +0900

    Initial commit

---
+++ prac3.py
@@ -0,0 +1,13 @@
+import string
+
+def get_idx_naive(word):
+    result = [-1]*len(string.ascii_lowercase)
+    for i in range(len(word)):
+        char = word[i]
+        for j in range(len(string.ascii_lowercase)):
+            lo = string.ascii_lowercase[j]
+            if result[j] == -1 and char == lo:
+                result[j] = i
+    print(' '.join([str(num) for num in result]))
+
+get_idx_naive("baeckjoon")
