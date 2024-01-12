51eaab6c17bc6e1b92c856ead5dc164a58a4b191
JungDaGyeom <ktkt1413@naver.com>
2024-01-12 18:20:46 +0900

    리트코드 순열 문 해설수정

--- cote/permutations.py
+++ cote/permutations.py
@@ -16,7 +16,7 @@ class Solution:
                     pair.append(i)
                     dfs(pair, n)
                     pair.pop()  #  백트레킹을 하기위한 과정, answer에 어펜드 된 i를 pair에서 제거해줌
-
+                                #  이 과정을 통해 첫번째 자리에 i로 시작하는 pair는 없게 됨
         dfs([],len(nums))  #  dfs 함수가 정의되기 전에 호출되면 NameError 발생할 수 있음
 
         return answer
