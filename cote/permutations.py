1dd9eab498783225a3f1aa46344a01e39f6e5290
JungDaGyeom <ktkt1413@naver.com>
2024-01-12 18:16:57 +0900

    리트코드 순열 문제

---
+++ cote/permutations.py
@@ -0,0 +1,22 @@
+#  leetcode46. 순열문제 permutations
+
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+        def dfs(pair, n):
+            if len(pair) == n:
+                answer.append(pair[:])
+                return
+
+            for i in nums:
+                if i not in pair:
+                    pair.append(i)
+                    dfs(pair, n)
+                    pair.pop()  #  백트레킹을 하기위한 과정, answer에 어펜드 된 i를 pair에서 제거해줌
+
+        dfs([],len(nums))  #  dfs 함수가 정의되기 전에 호출되면 NameError 발생할 수 있음
+
+        return answer
